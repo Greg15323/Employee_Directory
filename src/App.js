b5ae47c9aa3362c11b2e1d1ddda45bb12a8f8feb
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-03-06 04:59:52 -0700

    Debug final push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Employees from "./pages/Employees";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <div>
+         <Navbar />
+         <Wrapper>
+           <Route exact path={process.env.PUBLIC_URL + "/"} component={Employees} />
+           <Route exact path={"/19-employee-directory"} component={Employees} />
+         </Wrapper>
+         <Footer />
+       </div>
+    </Router>
   );
 }
-
 export default App;
